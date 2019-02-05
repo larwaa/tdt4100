34fe74c3714e55a30806c90a68a796d0faea9405
borgeha <borge@10.24.22.103>
2019-02-05 11:13:01 +0100

    Filmeksempel før forelesning.

---
+++ foreksempel/src/filmvisning/Filmvisning.java
@@ -0,0 +1,13 @@
+package filmvisning;
+
+public class Filmvisning {
+	
+	private String filmtittel;
+	private String sal;
+	private int alder;
+	private String nasjonalitet;
+	private String tid;
+	private int pris;
+	
+
+}
