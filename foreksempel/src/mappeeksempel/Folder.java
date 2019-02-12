afc4010c4523383cda0c712709f629316b2234a1
borgeha <borge@10.24.22.103>
2019-02-12 11:08:44 +0100

    Forberedelser til dagens dont.

--- foreksempel/src/mappeeksempel/Folder.java
+++ foreksempel/src/mappeeksempel/Folder.java
@@ -4,6 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Folder {
+	
+	
+	public static void main(String[] args) {
+//		Folder root = new Folder("Rot",null);
+//		File rotfil = new File("rotfil.txt",root);
+//		Folder tmp = new Folder("tmp",root);
+//		File tmpfil = new File("tmpfil.txt",tmp);
+//		Folder users = new Folder("users",root);
+//		Folde borgeh = new Folder("borgeh",users);
+//		File egenfil1 = new File("egenfil.txt", borgeh);
+//		File egenfil2 = new File("egenfil2.txt", borgeh);
+//		Folder div = new Folder("div",root);
+//		root.printContent();
+//		users.move(borgeh);
+//		root.printContent();
+
+	}
 
 
 }
