4db00db8176272d0980203e714269bc4a5a49302
borgeha <borge@10.24.22.103>
2019-02-19 16:02:17 +0100

    Gårsdagens enklere testeksempel, som noen ønsket art jeg la ut.

---
+++ foreksempel/src/filmvisning/AllTests.java
@@ -0,0 +1,12 @@
+package filmvisning;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ Film2Test.class, Filmvisning2Test.class, Kino2Test.class, Sal2Test.class })
+
+public class AllTests {
+
+}
