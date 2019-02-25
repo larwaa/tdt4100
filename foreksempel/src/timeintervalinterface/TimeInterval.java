1d32bf299d34389357568ff86baf5abe511b12d1
borgeha <borge@10.24.22.103>
2019-02-25 11:18:35 +0100

    Enda mer

---
+++ foreksempel/src/timeintervalinterface/TimeInterval.java
@@ -0,0 +1,10 @@
+package timeintervalinterface;
+
+public interface TimeInterval {
+
+	public int getStartHour();
+	public int getStartMin();
+	public int getEndHour();
+	public int getEndMin();
+	public int getDuration();
+}
