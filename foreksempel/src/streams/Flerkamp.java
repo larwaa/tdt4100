1c19e1744a99431a90f0bf040323d83d7181e578
borgeha <borge@10.24.22.103>
2019-03-06 16:31:04 +0100

    Endret litt. Typisk, innit.

--- foreksempel/src/streams/Flerkamp.java
+++ foreksempel/src/streams/Flerkamp.java
@@ -27,19 +27,19 @@ public class Flerkamp {
 		// Setter den sammen av folderen til classfilene
 		URI classUri = URI.create((getClass().getProtectionDomain().getCodeSource().getLocation().toString()+
 				getClass().getPackageName()));
-		System.out.println(classUri+"\n");
+		System.out.println(classUri);
 
 		
 		// Eksempel for å lese ut innholdet av en katalog.
 		Path path = Paths.get(classUri);
 		try(Stream<Path> subPaths = Files.walk(path)) {
-			subPaths.forEach(System.out::println);
+//			subPaths.forEach(System.out::println);
 		}
 		catch (Exception e) {
 			// TODO: handle exception
 		}
 
-		System.out.println("\n");
+		System.out.println();
 		
 		// Her kommer selve streams-metoden i bruk:
 		// Hente ut linje for linje fra en fil:
@@ -95,23 +95,22 @@ public class Flerkamp {
 
 		// Nå skal alle deltakerne ha blitt lagt inn i listen. Så en enkel stream-måte å skrive dem ut på,
 		// println kaller toString i hvert Deltakerobjekt på veien.
-		fk.deltakere.stream().forEach(System.out::println);
+//		fk.deltakere.stream().forEach(System.out::println);
 
 		// Hva med å skrive ut alle navnene til deltakerne? Tenk at data flyter fra venstre mot høyre og endres.
-		System.out.println(fk.deltakere.stream().map(x -> x.getName()).collect( Collectors.joining( ", " )));
+		System.out.println("Deltakere: "+fk.deltakere.stream().map(x -> x.getName()).collect( Collectors.joining( ", " )));
 		
 		// Hva med å finne deltakerne som fikk minst 10 poeng på ballongskyting og poker?
 		fk.deltakere.stream()  // Enhver List kan streames!
-		.filter(x -> x.getBaloonshooting() >= 10)
-		.filter(x -> x.getPoker() >= 10)
-		.forEach(p -> System.out.println("\nMinst to poeng i poker og ballongskyting: "+p.getName()));		
+		.filter(x -> x.getBaloonshooting() >= 10 && x.getPoker() >= 10)
+		.forEach(p -> System.out.println("\nMinst ti poeng i poker og ballongskyting: "+p.getName()));		
 		
 		// Så, for syns skyld lesing av filen på en av de gamle måtene:
-		try {
-			fk.readFile_old("C:/Users/borge/tdt4100-v2019-underviser/git/tdt4100-v2019-students/foreksempel/target/test-classes/streams/flerkamp.txt");
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
+		// Fjerne kommentarer under hvis du vil se den gamle måten å lese ut på:
+//		try {
+//			fk.readFile_old("C:/Users/borge/tdt4100-v2019-underviser/git/tdt4100-v2019-students/foreksempel/target/test-classes/streams/flerkamp.txt");
+//		} catch (FileNotFoundException e) {
+//			e.printStackTrace();
+//		}
 	}
-
 }
