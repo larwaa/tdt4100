cb18e7e4bf335d63331086cb9b7031d981faf9c9
borgeha <borge@10.24.22.103>
2019-03-11 22:29:00 +0100

    "forpening"

--- foreksempel/src/streams/StreamExamples.java
+++ foreksempel/src/streams/StreamExamples.java
@@ -25,19 +25,19 @@ public class StreamExamples {
 		List<Integer> liste = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
 
 		// Fjerne oddetall:
-		System.out.print("Liste av partall:");
+		System.out.print("\nListe av partall:");
 		System.out.println(liste.stream()
 				.filter(i -> i%2 == 0)
 				.collect(Collectors.toList()));
 
 		// Summer tall:
-		System.out.print("Sum av tall: ");
+		System.out.print("\nSum av tall: ");
 		System.out.println(liste.stream()
 				.reduce(0, (sum, i) -> sum + i));
 
 		List<String> people = Arrays.asList("Al", "Skybert", "Farfar", "Farmor", "Håvard", "Jørn", "Andrea", "Børge");
 		// Liste med lengde av Stringobjekter:
-		System.out.print("Navnelengder: ");
+		System.out.print("\nNavnelengder: ");
 		System.out.println(people.stream()
 				.map(s -> s.length())
 				//		.distinct()
@@ -46,14 +46,14 @@ public class StreamExamples {
 
 
 		// Sum av lengder av Stringobjekter:
-		System.out.print("Sum av navnelengder: ");
+		System.out.print("\nSum av navnelengder: ");
 		System.out.println(people.stream()
 				.map(s -> s.length())
 				.reduce(0, (sum, i) -> sum+i));
 
 
 		// Fjerne de første seks elementene i en intstream fra 0 til 10, og telle elementer:
-		System.out.print("Hopper over noen elementer: ");
+		System.out.print("\nHopper over noen elementer: ");
 		System.out.println(IntStream
 				.range(0, 10)
 				//				.peek(System.out::println)
@@ -61,7 +61,7 @@ public class StreamExamples {
 				.count());
 
 		// Legge sammen alle tall opp til 200 som går opp i tre og sju
-		System.out.print("Legge sammen elementer fra 0-200 som går opp i 3 og 7: ");
+		System.out.print("\nLegge sammen elementer fra 0-200 som går opp i 3 og 7: ");
 		System.out.println(IntStream
 				.range(0, 200)
 				.filter(t -> t%3 == 0)
@@ -72,7 +72,7 @@ public class StreamExamples {
 
 		// Lage en kontinuerlig strøm av heltall som deles på et annet tall, og så skrive ut de første 100:
 		// Håvard sitt forslag
-		System.out.println("Bare et skrudd eksempel på hva en kan. Limit er bra, ellers ville den fortsatt...");
+		System.out.println("\nBare et skrudd eksempel på hva en kan. Limit er bra, ellers ville den fortsatt...");
 		IntStream
 		.range(0,999999999)
 		//		.mapToDouble(n -> Double.valueOf(n)) // Er det samme, men mer tungvinte enn
@@ -85,7 +85,7 @@ public class StreamExamples {
 
 		List<String> folk = Arrays.asList("Al", "Skybert", "Farfarharetlangtnavn", "Farmor", "Håvard", "Jørn", "Andrea", "Børge");
 
-		System.out.println("Personer med navn lenger enn fem tegn, på en fin måte:");
+		System.out.println("\nPersoner med navn lenger enn fem tegn, på en fin måte:");
 		// Skrive ut alle navn i listen som er mer enn fem tegn, og hvor mange tegn de er.
 		folk.stream()
 		//		.peek(System.out::println)
@@ -94,7 +94,7 @@ public class StreamExamples {
 		.forEach(System.out::println);
 
 		// Lage en samling som inneholder alle personene som starter med "A" og lengde over fire.
-		System.out.print("Navn som starter med F og lengde over 6: ");
+		System.out.print("\nNavn som starter med F og lengde over 6: ");
 		System.out.println(folk.stream()
 				.filter(p -> p.startsWith("F"))
 				.filter(p -> p.length() > 6)
@@ -103,7 +103,7 @@ public class StreamExamples {
 
 		// Lese fra fil og legge inn i objekter: se Flerkamp.java
 
-		System.out.println("Lese fra fil:");
+		System.out.println("\nLese fra fil:");
 		// Lese fra fil:
 		// Hvis det ikke hadde vært i main kune en brukt getClass.getResource 
 		// Merk spesialtilfelle. På denne måten MÅ en ikke innkapsle i try. Det må en uten streams.
