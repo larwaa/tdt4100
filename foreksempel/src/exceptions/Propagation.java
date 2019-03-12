202a0519de3db9338414289f7841c49d3449b8ca
borgeha <borge@10.24.22.103>
2019-03-12 12:07:24 +0100

    Eksempelet vi skal kverne igjennom på forelesning.

--- foreksempel/src/exceptions/Propagation.java
+++ foreksempel/src/exceptions/Propagation.java
@@ -1,30 +1,34 @@
 package exceptions;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Propagation {
 
 
 	static void method4() {
 		int a = 7;
-		int b = 0;
-		int c = a/b;
-		System.out.println("Inni method4");
+		int b = 1;
+		int c = a / b;
+
+		System.out.println("Inni method4: "+c);
 	}
-	
+ 
 	static void method3() {
 		method4();
 		System.out.println("Inni method3");
 	}
-	
+
 	static void method2() {
 		method3();
 		System.out.println("Inni method2");
 	}
-	
+
 	static void method1() {
 		method2();
 		System.out.println("Inni method1");
 	}
-	
+
 	static void method() {
 		method1();
 		System.out.println("Inni method");
@@ -32,7 +36,26 @@ public class Propagation {
 
 
 	public static void main(String[] args) {
+		
 		method();
-	}
+		System.out.println("Inni main");
+        }
 
 }
+
+
+//File file = new File("src/exceptions/Propagation.java"); 
+//try {
+//	BufferedReader br = new BufferedReader(new FileReader(file));
+//	br.readLine();
+//	String str = "";
+//
+//	while ((str = br.readLine()) != null) 
+//		System.out.println(str);
+//	br.close();
+//
+//} catch (Exception e) {
+//	// TODO Auto-generated catch block
+//	e.printStackTrace();
+//} 
+//
