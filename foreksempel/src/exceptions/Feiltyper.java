1daee300a44ca20a3b80365c17f358520fddeb0a
borgeha <borge@10.24.22.103>
2019-03-13 12:08:23 +0100

    ny?

--- foreksempel/src/exceptions/Feiltyper.java
+++ foreksempel/src/exceptions/Feiltyper.java
@@ -4,16 +4,18 @@ import java.util.Map;
 
 public class Feiltyper {
 
+
+		// Diverse feiltyper!
 	public static void main(String[] args) {
 
 
 		// Nullpointerexception:
-//				String foo = null;
-//		        int length = foo.length();   
+		//				String foo = null;
+		//		        int length = foo.length();   
 
 		// Classcastexception:
-//		        Object i = Integer.valueOf(42);
-//		        String s = (String)i; 
+		//		        Object i = Integer.valueOf(42);
+		//		        String s = (String)i; 
 
 		// Indexoutofboundserror:
 		//		List<String> ls=new ArrayList<>();
@@ -22,25 +24,25 @@ public class Feiltyper {
 		//	      ls.get(3);
 
 		// nosuchelementexception:
-//				List<String> ls = new ArrayList<>();
-//				Iterator<String> it = ls.iterator();
-//				System.out.println(it.next());
+		//				List<String> ls = new ArrayList<>();
+		//				Iterator<String> it = ls.iterator();
+		//				System.out.println(it.next());
 
 		// UnsupportedOperationException
-//		String[] flowers = { "Ageratum", "Allium", "Poppy", "Catmint" }; 
-////		List<String> flowerList = Arrays.asList(flowers);  
-//		List<String> flowerList = new ArrayList<>(Arrays.asList(flowers));
-//		flowerList.add("Celosia");
-//		flowerList.forEach(System.out::println);
-		
-		
+		//		String[] flowers = { "Ageratum", "Allium", "Poppy", "Catmint" }; 
+		////		List<String> flowerList = Arrays.asList(flowers);  
+		//		List<String> flowerList = new ArrayList<>(Arrays.asList(flowers));
+		//		flowerList.add("Celosia");
+		//		flowerList.forEach(System.out::println);
+
+
 		// IllegalArgumentException:
-//		Map<String, Integer> tempMap = Map.of(
-//		        "Ost",    13,
-//		        "Pølse",      19,
-//		        "Ost",    13, // !
-//		        "Bacalao",      14
-//		);
-//	}
+		//		Map<String, Integer> tempMap = Map.of(
+		//		        "Ost",    13,
+		//		        "Pølse",      19,
+		//		        "Ost",    13, // !
+		//		        "Bacalao",      14
+		//		);
+	}
 
 }
